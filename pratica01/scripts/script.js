f31b81d4422c6cb2257640aea0e8fc278a8009ad
Filipe <filipegallodev@hotmail.com>
2021-11-11 21:01:55 -0300

    register-page

---
+++ pratica01/scripts/script.js
@@ -0,0 +1,15 @@
+function mouseDentro() {
+   var botao = document.getElementById('botao')
+   botao.style.background = "rgb(0, 65, 117)"
+   botao.style.width = "280px"
+   botao.style.height = "65px"
+   botao.style.marginBottom = "10px"
+}
+
+function mouseFora() {
+   var botao = document.getElementById('botao')
+   botao.style.background = "rgb(5, 108, 192)"
+   botao.style.width = "270px"
+   botao.style.height = "60px"
+   botao.style.marginBottom = "15px"
+}
